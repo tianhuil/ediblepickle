2b786621be68332b128236df903687eeb8444947
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-09-02 10:23:41 -0400

    Bumping version to 1.2.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ CLASSIFIERS = [
 
 settings.update(
     name='ediblepickle',
-    version='1.1.3',
+    version='1.2.0',
     description='Checkpoint',
     long_description=open('README.rst').read(),
     author='Pavan Mallapragada',
